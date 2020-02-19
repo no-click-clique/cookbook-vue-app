38e0aaba86ba5709fac61f990970c48f6c8bd008
dani <dani.zaghian@gmail.com>
2020-02-19 09:51:36 -0800

    adds axios request to index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p>{{ recipes }}</p>
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
+      <p>Prep time: {{ recipe.formatted.prep_time }}</p>
+    </div>
   </div>
 </template>
 
@@ -9,14 +14,23 @@
 </style>
 
 <script>
+// var axios = require("axios");
+import axios from 'axios';
 export default {
   data: function() {
     return {
       message: "Hello World",
-      recipes: ["Pizza", "Peanut Butter Cookies", "Empty Sandwich"]
+      recipes: []
     };
   },
-  created: function() {},
+  // respone = HTTP.get("/api/recipes")
+  // @recipes = response.parse
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      console.log(response.data);
+      this.recipes = response.data;
+    });
+  },
   methods: {}
 };
 </script>
