cc703a1aaa0504616dcd6430f5fd9943d8af6a30
dani <dani.zaghian@gmail.com>
2020-02-24 10:46:38 -0800

    adds authentication

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Test from '../views/Test.vue';
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter)
 
@@ -21,6 +24,21 @@ const routes = [
     path: '/test',
     name: 'Test',
     component: Test
+  },
+  { 
+    path: "/signup", 
+    name: "Signup", 
+    component: Signup 
+  },
+  { 
+    path: "/login", 
+    name: "Login", 
+    component: Login 
+  },
+  { 
+    path: "/logout", 
+    name: "Logout", 
+    component: Logout 
   }
 ]
 
