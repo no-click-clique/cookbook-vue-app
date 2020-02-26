5941900bbaa597b150a65b8d90641a487148b58f
dani <dani.zaghian@gmail.com>
2020-02-26 09:58:26 -0800

    adds bootstrap and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> | 
-      <router-link to="/test">Test</router-link> |
-      <router-link to="/signup">Signup</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/logout">Logout</router-link> |
-      <router-link to="/recipes">All Recipes</router-link> |
-      <router-link to="/recipes/new">New Recipe</router-link> 
+
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Cookbook</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/logout">Logout</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/recipes">All Recipes</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/recipes/new">New Recipe</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <!-- every individual view in the views file gets interpolated here -->
+      <router-view/>
     </div>
-    <router-view/>
+
   </div>
 </template>
 
-<style>
+<!-- <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -35,4 +60,4 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-</style>
+</style> -->
